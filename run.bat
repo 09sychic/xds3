a0e8463a02a0e4856c381ee962aacee638904102
Drae <bwakang1212@gmail.com>
2025-09-15 13:17:22 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,9 @@
 @echo off
-set "RAWURL=https://is.gd/sortps1"
-powershell -NoProfile -Command ^
-  "Try {
-      Invoke-WebRequest -Uri '%RAWURL%' -OutFile '%~dp0sqd5.ps1' -UseBasicParsing -ErrorAction Stop;
-      Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0sqd5.ps1\"' -Verb RunAs
-  } Catch {
-      Write-Error 'Download failed';
-      Exit 1
-  }"
+set "ps1url=https://raw.githubusercontent.com/09sychic/xds3/refs/heads/main/sort.ps1"
+set "ps1file=%temp%\sort.ps1"
+
+REM Elevate and run PS1
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"Start-Process PowerShell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"Invoke-WebRequest -UseBasicParsing %ps1url% -OutFile %ps1file%; & %ps1file%; Remove-Item %ps1file%\"'"
+
+exit /b
